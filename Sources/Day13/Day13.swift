8854e4d4df02ad7243bd10fb8d3a234e545936e8
Moritz Sternemann <opensource@moritzsternemann.de>
2023-12-14 16:37:44 +0100

    Day 13

--- Sources/Day13/Day13.swift
+++ Sources/Day13/Day13.swift
@@ -1,15 +1,64 @@
+import Algorithms
+
 struct Day13: Solution {
     static let day = 13
 
+    private let patterns: [Substring]
+
     init(input: String) {
-
+        self.patterns = input.split(separator: "\n\n")
     }
 
-    func runPartOne() -> String {
-        "not implemented"
+    func runPartOne() -> Int {
+        solve(desiredDifferenceCount: 0)
     }
 
-    func runPartTwo() -> String {
-        "not implemented"
+    func runPartTwo() -> Int {
+        solve(desiredDifferenceCount: 1)
+    }
+
+    private func solve(desiredDifferenceCount: Int) -> Int {
+        patterns
+            .map { pattern in
+                let rowMap = pattern.split(separator: "\n")
+                    .map { row in
+                        row.map { $0 }
+                    }
+                let columnMap = rowMap.transposed()
+
+                return findMirror(in: columnMap, desiredDifferenceCount: desiredDifferenceCount)
+                    + findMirror(in: rowMap, desiredDifferenceCount: desiredDifferenceCount) * 100
+            }
+            .sum()
+    }
+
+    private func findMirror(in pattern: [[Character]], desiredDifferenceCount: Int) -> Int {
+        let patternKeyRange = 0..<pattern.count
+        
+        return pattern.enumerated()
+            .adjacentPairs()
+            .filter { adjacent -> Bool in
+                // Number of different characters between both pair elements
+                let adjacentDifferenceCount = zip(adjacent.0.element, adjacent.1.element)
+                    .filter { $0.0 != $0.1 }
+                    .count
+
+                // Iterate outwards (- and +) from the current adjacent pair and
+                // filter out elements where either before or after are outside
+                // the range of all elements
+                let potentialMirrorOutwardPairs = (0..<adjacent.0.offset)
+                    .map { (adjacent.0.offset - $0 - 1, adjacent.1.offset + $0 + 1) }
+                    .filter { patternKeyRange.contains($0.0) && patternKeyRange.contains($0.1) }
+
+                // Sum of the number of different characters between each of the outwards pairs
+                let outwardPairsDifferenceCount = potentialMirrorOutwardPairs
+                    .flatMap { zip(pattern[$0.0], pattern[$0.1]) }
+                    .filter { $0.0 != $0.1 }
+                    .count
+
+                return adjacentDifferenceCount + outwardPairsDifferenceCount == desiredDifferenceCount
+            }
+            .map { $0.1.offset }
+            .max() ?? 0
     }
 }
--- Tests/Day13/Day13Tests.swift
+++ Tests/Day13/Day13Tests.swift
@@ -5,10 +5,10 @@ final class Day13Tests: XCTestCase, SolutionTest {
     typealias SUT = Day13
 
     func testPartOne() throws {
-        try XCTAssertEqual(sut.runPartOne(), "not implemented")
+        try XCTAssertEqual(sut.runPartOne(), 405)
     }
 
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.runPartTwo(), "not implemented")
+        try XCTAssertEqual(sut.runPartTwo(), 400)
     }
 }
