f61aacff3267fa7e0f9a290a8c3c0a82b49f33f0
Moritz Sternemann <opensource@moritzsternemann.de>
2024-01-14 23:56:32 +0100

    Day 18

--- Sources/Day18/Day18.swift
+++ Sources/Day18/Day18.swift
@@ -1,15 +1,60 @@
+import Algorithms
+
 struct Day18: Solution {
     static let day = 18
 
+    typealias Instruction = (direction: Direction, length: Int, directionByColor: Direction, lengthByColor: Int)
+    private let digPlan: [Instruction]
+
     init(input: String) {
+        self.digPlan = input.split(separator: "\n")
+            .compactMap { line -> Instruction? in
+                guard let (_, dir, num, col) = line.wholeMatch(of: #/(.) (\d+) \(#([0-9a-f]{6})\)/#)?.output,
+                      let direction = dir.first.flatMap({ Direction(directionCode: $0) }),
+                      let length = Int(num),
+                      let directionByColor = Self.parseDirection(col.last?.wholeNumberValue),
+                      let lengthByColor = Int(col.prefix(5), radix: 16)
+                else { return nil }
 
+                return (direction, length, directionByColor, lengthByColor)
+            }
     }
 
-    func runPartOne() -> String {
-        "not implemented"
+    private static func parseDirection(_ number: Int?) -> Direction? {
+        return switch number {
+        case 0: .east
+        case 1: .south
+        case 2: .west
+        case 3: .north
+        default: nil
+        }
     }
 
-    func runPartTwo() -> String {
-        "not implemented"
+    func runPartOne() -> Int {
+        solve { point, instruction in
+            point.moved(into: instruction.direction, by: instruction.length)
+        }
+    }
+
+    func runPartTwo() -> Int {
+        solve { point, instruction in
+            point.moved(into: instruction.directionByColor, by: instruction.lengthByColor)
+        }
+    }
+
+    private func solve(move: (Point, Instruction) -> Point) -> Int {
+        var route: [Point] = []
+        var outlineLength = 0
+        var current = Point.zero
+        for instruction in digPlan {
+            let instructionEnd = move(current, instruction)
+            route.append(instructionEnd)
+            outlineLength += current.distance(to: instructionEnd)
+            current = instructionEnd
+        }
+
+        return outlineLength / 2 + route.adjacentPairs()
+            .map { ($0.0.y + $0.1.y) * ($0.0.x - $0.1.x) }
+            .sum() / 2 + 1
     }
 }
--- Tests/Day18/Day18Tests.swift
+++ Tests/Day18/Day18Tests.swift
@@ -5,10 +5,10 @@ final class Day18Tests: XCTestCase, SolutionTest {
     typealias SUT = Day18
 
     func testPartOne() throws {
-        try XCTAssertEqual(sut.runPartOne(), "not implemented")
+        try XCTAssertEqual(sut.runPartOne(), 62)
     }
 
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.runPartTwo(), "not implemented")
+        try XCTAssertEqual(sut.runPartTwo(), 952408144115)
     }
 }
